9cb0495a0e610f2d0ab32550cdeb466a9f76cbc8
Ivan Kalashnikov <ivank.public@gmail.com>
2024-02-06 19:44:59 +0700

    (dev) - dadata suggestions integration, refactoring, README report.

---
+++ task_2/src/components/feedback-form.vue
@@ -0,0 +1,109 @@
+<template>
+  <h2>Форма обратной связи</h2>
+  <form @submit.prevent="submitData">
+    <div class="form-group">
+      <label for="name">ФИО:</label>
+      <dadata-suggestions
+        :model-value="name"
+        @update:model-value="name = $event"
+        token="212e4b10cdd231d52c3d37ffb0d16efc4e75c6fd"
+        type="NAME"
+        fieldValue="value"
+        placeholder="Обязательно для заполнения"
+        required
+      />
+    </div>
+    <div class="form-group">
+      <label for="address">Адрес:</label>
+      <dadata-suggestions
+        :model-value="address"
+        @update:model-value="address = $event"
+        token="212e4b10cdd231d52c3d37ffb0d16efc4e75c6fd"
+        type="ADDRESS"
+        fieldValue="value"
+      />
+    </div>
+    <div class="form-group">
+      <label for="phone">Мобильный телефон:</label>
+      <input type="tel" id="phone" v-model="phone" placeholder="Обязательно для заполнения" required>
+    </div>
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" id="email" v-model="email">
+    </div>
+    <div class="form-group">
+      <label for="comment">Комментарий:</label>
+      <textarea id="comment" v-model="comment"></textarea>
+    </div>
+    <button type="submit">Отправить</button>
+  </form>
+  <!-- name : <br />
+  {{ name }} -->
+</template>
+
+<script>
+  import DadataSuggestions from './dadata-suggestions';
+
+  export default {
+    name: 'feedback-form',
+    components: {
+      DadataSuggestions
+    },
+    data() {
+      return {
+        name: '',
+        address: '',
+        phone: '',
+        email: '',
+        comment: ''
+      };
+    },
+    methods: {
+      submitData() {
+        console.log(this.name, this.address, this.phone, this.email, this.comment);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  max-width: 400px;
+  margin: 0 auto;
+}
+
+.form-group {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 0.5rem;
+}
+
+label {
+  font-weight: bold;
+  text-align: left;
+  margin-right: 0.4rem;
+}
+
+input, textarea {
+  padding: 0.5rem;
+  font-size: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 6px;
+}
+
+button {
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+</style>
