1191012fd10329cf881211b073ae05700aa26a36
Ivan Kalashnikov <ivank.public@gmail.com>
2024-02-19 13:42:46 +0700

    (fix) - tab size.

--- task_3_final/publishing/views/site/index.php
+++ task_3_final/publishing/views/site/index.php
@@ -6,10 +6,10 @@ $this->title = 'Publishing house';
 ?>
 
 <div class="site-index">
-    <div class="body-content">
-        <div class="books-content">
-            <h2> All books info</h2>
-            <?= $this->render('/books/all-books-info-grid', ['books' => $books]) ?>
-        </div>
+  <div class="body-content">
+    <div class="books-content">
+      <h2> All books info</h2>
+      <?= $this->render('/books/all-books-info-grid', ['books' => $books]) ?>
     </div>
+  </div>
 </div>
